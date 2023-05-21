09e3c9194cf4f6420c0fbece4c0a8b99332d5832
marylambtech <marylambtech@gmail.com>
2023-05-21 15:24:29 -0700

    This file contains a C program that prints the size of various types on the computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints the size of various types on my computer
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+sizeof(int);
+sizeof(char);
+sizeof(long int);
+sizeof(long long int);
+sizeof(float);
+sizeof(double);
+return (0);
+}
