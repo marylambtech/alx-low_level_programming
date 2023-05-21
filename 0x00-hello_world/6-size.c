f72e1f5a19c90a40efe34856ea5ce0079239e3e2
marylambtech <marylambtech@gmail.com>
2023-05-21 15:53:12 -0700

    This file contains a C program that prints the size of various types on the computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,5 @@ printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", sizeof(float));
-printf("Size of a double: %d byte(s)\n", sizeof(double));
 return (0);
 }
