c370d3d3b9107bf5797e39dc7e38c306bae59aed
marylambtech <marylambtech@gmail.com>
2023-05-21 15:02:03 -0700

    This file contains C program that prints a sentence using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a sentence using the printf function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("\"with proper grammar, but the outcome is a piece of art'");
+return (0);
+}
