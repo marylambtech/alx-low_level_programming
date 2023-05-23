58714bf32c48d5f342f0a00817bc115012b8a442
marylambtech <marylambtech@gmail.com>
2023-05-23 03:20:15 -0700

    This file contains program that prints digits 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - This program prints the numbers 0-9
+ *By Mariam A. Ibrahim
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int k;
+k = 0;
+while (k < 10)
+{
+printf("%d", k);
+k++;
+}
+printf("\n");
+return (0);
+}
