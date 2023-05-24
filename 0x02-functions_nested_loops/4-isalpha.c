0c0b6c6d2958a9d88c47468125df36e286ae30d5
marylambtech <marylambtech@gmail.com>
2023-05-24 14:27:03 -0700

    This file contains function that checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ *_isalpha - This file checks for lowercas or uppercase alphabet
+ *@c: character type
+ *Description: Returns 1 if c isan alphabet, Returns 0 otherwise
+ *
+ *By Mariam A. Ibrahim
+ *
+ *Return: 0 (Success)
+ */
+int _isalpha(int c)
+{
+c = 'a' || 'A';
+while (c <= 'z' || c <= 'Z')
+c++;
+{
+if (isalpha(c))
+return (1);
+else
+return (0);
+}
+}
