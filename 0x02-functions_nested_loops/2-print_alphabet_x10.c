268418cfbc783baa08579270f6ae91086068c182
marylambtech <marylambtech@gmail.com>
2023-05-24 07:41:17 -0700

    This file contains unction that prints 10 times the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - This file contains function that prints
+ *10 times the alphabet, in lowercase, followed by a new line
+ *Description: _putchar can only be used twice in the code
+ *
+ *By Mariam A. Ibrahim
+ *
+ *Return: 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+char k;
+int count;
+for (count = 0; count < 10; count++)
+{
+for (k = 'a'; k <= 'z'; k++)
+{
+_putchar(k);
+}
+}
+_putchar('\n');
+}
