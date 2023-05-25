e392c043394a4b00851860977de6be10c6ad8ba6
marylambtech <marylambtech@gmail.com>
2023-05-24 19:29:32 -0700

    This file prints sign of an integer

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -20,13 +20,13 @@ if (n > '0')
 _putchar('+');
 return (1);
 }
-else if (n == 48)
+else if (n < '0')
 {
-_putchar(48);
-return (0);
+_putchar('-');
+return (-1);
 }
 else
-{_putchar('-');
-return (-1);
+{_putchar(48);
+return (0);
 }
 }
