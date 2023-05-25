e3154002dc50bf4f65343d2d167ae358776def03
marylambtech <marylambtech@gmail.com>
2023-05-24 20:08:35 -0700

    This file computes an absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_abs - This file computes the absolute value of an integer
+ *@n: integer type value
+ *Description: prints absolute value
+ *
+ *By Mariam A. Ibrahim
+ *
+ *Return: 0 (Success)
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+else
+{
+return (-n);
+}
+}
