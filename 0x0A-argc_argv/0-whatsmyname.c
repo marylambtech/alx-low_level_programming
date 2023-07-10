6f5bff4362c9a81fc57759046284962885b6ddb9
marylambtech <marylambtech@gmail.com>
2023-07-10 08:47:27 -0700

    This file contains program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the program name.
+ * @argc: The number of arguments passed.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always o.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
