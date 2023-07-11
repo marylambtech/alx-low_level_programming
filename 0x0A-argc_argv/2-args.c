298d00ccbc13fb1dd1c61c4f8a0118a63d4916f2
marylambtech <marylambtech@gmail.com>
2023-07-11 01:51:45 -0700

    This file contains program that prints arguments passed to it in a line

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints all arguments received.
+ * @argc: The number of arguments passed.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always o.
+ */
+
+int main(int argc, char *argv[])
+{
+int x;
+for (x = 0; x < argc; x++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
