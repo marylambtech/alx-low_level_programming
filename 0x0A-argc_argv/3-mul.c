e2448818a39bf79404946f8e24a0c8f660b521f1
marylambtech <marylambtech@gmail.com>
2023-07-11 05:51:27 -0700

    This file contains program that prints the result of the multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,16 +13,19 @@
 
 int main(int argc, char *argv[])
 {
-int num1, num2, result;
-if (argc != 3)
-{
-printf("Error\n");
-return (1);
-}
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-result = num1 * num2;
+	int num1, num2, prod;
 
-printf("%d\n", result);
-return (0);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	prod = num1 * num2;
+
+	printf("%d\n", prod);
+
+	return (0);
 }
