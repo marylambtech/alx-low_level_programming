20b9cadae0027fb6bcd6ecc831a7e630d3684ef0
marylambtech <marylambtech@gmail.com>
2023-07-19 01:43:59 -0700

    argument type correction

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,6 +15,6 @@ return;
 }
 
 printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
-printf("Age: %d\n", d->age);
+printf("Age: %f\n", d->age);
 printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 }
