d1b2dd00038e570a54eae8fd1b1dc4e2e6988261
marylambtech <marylambtech@gmail.com>
2023-07-27 18:26:18 -0700

    corrections made

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,14 +18,14 @@ list_t *add_node(list_t **head, const char *str)
 	while (str[len])
 		len++;
 
-	new = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 		return (NULL);
 
 	new_node->str = strdup(str);
 	new_node->len = len;
-	new_node->next = (*head);
-	(*head) = new_node;
+	new_node->next = *head;
+	*head = new_node;
 
 	return (*head);
 }
