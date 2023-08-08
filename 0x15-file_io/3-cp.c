8f01015298864c8ccfd73bfc7df376fcdcfdbb35
marylambtech <marylambtech@gmail.com>
2023-08-07 20:51:37 -0700

    This file contains program that copies the content of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,99 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+char *allocate_buffer(char *filename);
+void end_file(int fd);
+
+/**
+ * allocate_buffer - Allocates 1024 bytes to a buffer.
+ * @filename: name of the file.
+ * Return: contents of a file.
+ */
+
+char *allocate_buffer(char *filename)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", filename);
+		exit(99);
+	}
+	return (buffer);
+}
+
+/**
+ * end_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+
+void end_file(int fd)
+{
+	int k;
+
+	k = close(fd);
+
+	if (k == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied.
+ * @argv: An array of pointers.
+ * Return: 0 on success.
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int from, to, r, w;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	buffer = allocate_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		w = write(to, buffer, r);
+
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+					"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
+	free(buffer);
+	end_file(from);
+	end_file(to);
+	return (0);
+}
